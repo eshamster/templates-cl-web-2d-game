a71720ad9f21709013cc60b761360ed4944c78d0
eshamster <hamgoostar@gmail.com>
2019-03-23 03:50:35 +0000

    Update: Use with-ecs-entity-parent pattern in main state

--- simple/game/state/main.lisp
+++ simple/game/state/main.lisp
@@ -5,10 +5,18 @@
         :cl-web-2d-game))
 (in-package :{{name}}/game/state/main)
 
-(def-game-state main ()
+(def-game-state main ((parent (make-ecs-entity)))
   :start-process
-  (state-lambda ()
+  (state-lambda (parent)
+    (with-ecs-entity-parent (parent)
+      )
     t)
   :process
-  (state-lambda ()
-    nil))
+  (state-lambda (parent)
+    (with-ecs-entity-parent (parent)
+      )
+    nil)
+  :end-process
+  (state-lambda (parent)
+    (delete-ecs-entity parent)
+    t))
